0707c2ac2cd6d480dd8d8877cbe3cb6f5aa2a51a
ronakgithub81 <46760686+ronakgithub81@users.noreply.github.com>
2019-01-30 16:27:36 -0500

    Update StudentDemo.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -15,6 +15,7 @@ public class StudentDemo
   private String name;
   private String program;
   private String demo;
+  //adding some comment
   /**
    * A constructor to pass in the given ID and then name
    * @param givenID - the ID to assign
